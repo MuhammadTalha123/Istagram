e4a8c1c33edcb20ba412342bdf9d0e88d9746885
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-06-12 01:00:59 +0500

    update

--- app/src/main/java/com/example/istagram/MainActivity.java
+++ app/src/main/java/com/example/istagram/MainActivity.java
@@ -2,15 +2,21 @@ package com.example.istagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
-    public void login(View viwe){
+    SharedPreferences sharedPreferences;
+
+    public void login(View view){
 
         EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
 
@@ -23,11 +29,19 @@ public class MainActivity extends AppCompatActivity {
         Log.i("password", passwordEditText.getText().toString());
 
         Toast.makeText(this, "Welcome! " + usernameEditText.getText().toString() + " your password is " + passwordEditText.getText().toString(), Toast.LENGTH_LONG).show();
+
+
     }
+
+    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        
     }
 }
